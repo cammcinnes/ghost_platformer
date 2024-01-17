0198070ec644befaf559f7b6f999cfd5f836d6bc
Cam McInnes <camtmcinnes@gmail.com>
2024-01-17 09:14:59 -0800

    start making player movement and use polygons for platforms

---
+++ scripts/world/world.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@onready var collision_polygon_2d = $platform/CollisionPolygon2D
+
+@onready var polygon_2d = $platform/CollisionPolygon2D/Polygon2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	polygon_2d.polygon = collision_polygon_2d.polygon
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
